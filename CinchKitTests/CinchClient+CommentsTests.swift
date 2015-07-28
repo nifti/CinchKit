7e4b3837d40b95b204844cb1afd4d028e28ceba5
Mikhail Vetoshkin <mvetoshkin@gmail.com>
2015-07-28 19:31:20 +0300

    remove comments

--- CinchKitTests/CinchClient+CommentsTests.swift
+++ CinchKitTests/CinchClient+CommentsTests.swift
@@ -33,5 +33,46 @@ class CinchClientCommentsSpec: QuickSpec {
                 }
             }
         }
+
+        describe("create & remove comments") {
+            let c = CinchClient()
+            CinchKitTestsHelper.setTestUserSession(c)
+
+            var newCommentUrl: NSURL?
+            let messagesUrl = NSURL(string: "http://identity-service-izygw8dtme.elasticbeanstalk.com/discussions/1eb66fe4-fef8-4897-a966-28f206d2e426/messages")!
+
+            it("should create comment") {
+                waitUntil(timeout: 5) { done in
+                    c.refreshSession { (account, error) in
+                        let params = [
+                            "message": "Test test test"
+                        ]
+
+                        c.createComment(atURL: messagesUrl, params: params, queue: nil, completionHandler: { (response, error) -> () in
+                            expect(error).to(beNil())
+                            expect(response).notTo(beNil())
+
+                            expect(response!.comments!.count).to(equal(1))
+
+                            newCommentUrl = response!.comments!.first!.href
+
+                            done()
+                        })
+                    }
+                }
+            }
+
+            it("should remove comment") {
+                waitUntil(timeout: 5) { done in
+                    c.refreshSession { (account, error) in
+                        c.removeComment(atURL: newCommentUrl!, queue: nil, completionHandler: { (response, error) -> () in
+                            expect(error).to(beNil())
+
+                            done()
+                        })
+                    }
+                }
+            }
+        }
     }
 }
